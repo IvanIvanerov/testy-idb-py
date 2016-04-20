840e44ad0fac0646ecb605210f4991af997e6b97
IvanIvanerov <ivanero123@gmail.com>
2016-04-20 11:21:31 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -37,7 +37,7 @@ for x in range(1, 50, 1):
     pn = root.cssselect('pn')
     price = root.cssselect('p_price')
     articul = root.cssselect('articul')
-     for i in range(1,len(brand),1):
+    for i in range(1,len(brand),1):
         print brand + " " + pn + " " + price + " " + articul
         scraperwiki.sqlite.save(unique_keys=["pk"], data={
                 'pk': index,
